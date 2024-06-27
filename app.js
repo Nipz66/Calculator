892732dc13f310df72e40c99753c4055b56bf0e9
Nipun Anupama <anupamaalahakoon66@gmail.com>
2024-06-27 15:26:01 +0530

    fix 01

--- app.js
+++ app.js
@@ -4,15 +4,30 @@
 
 // console.log(typeof name);
 // console.log(typeof age);
-
+ 
 
 
 function calc() {
-    let number1 = document.getElementById("number01").value;
-    let number2 = document.getElementById("number02").value;
-    let lblOutput = document.getElementByI("output");
 
-    lblOutput.innerHTML=(number1+number2);
+    let number01;
+    let number02;
+
+    number01 = new Number(document.getElementById("number01").value);
+    number02 = new Number(document.getElementById("number02").value);
+
+    let op = document.getElementById("op").value;
+    let lblOutput = document.getElementById("output");
+
+    console.log(number01+number02);
+
+    switch (op) {
+        case "+":lblOutput.innerHTML=number01+number02;break;
+    }
+
+    console.log(typeof number01);
+    console.log(typeof number02);
+
+
 
 
 }
